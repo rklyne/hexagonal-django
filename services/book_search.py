4aff2835bce38675ac2606ff544b5876bb634208
Ronan Klyne <ronan.klyne@virtualstock.co.uk>
2019-01-30 10:50:53 +0000

    Add book search service

---
+++ services/book_search.py
@@ -0,0 +1,37 @@
+import attr
+
+
+class SearchService(object):
+    class SearchException(Exception):
+        pass
+
+    class NoResults(SearchException):
+        pass
+
+    class MultipleResults(SearchException):
+        pass
+
+    def _one_from_iterator(self, iterator):
+        try:
+            return iterator.next()
+        except StopIteration:
+            raise self.NoResults
+        finally:
+            try:
+                iterator.next()
+            except StopIteration:
+                pass
+            else:
+                raise self.MultipleResults
+
+
+@attr.s
+class BookSearchService(SearchService):
+    book_repo = attr.ib()
+
+    def get_by_title(self, title):
+        books = self.book_repo.iter_by_title(title=title)
+        return self._one_from_iterator(books)
+
+    def list_by_author(self, author):
+        return self.book_repo.iter_by_author(author)
---
+++ services/test_book_search.py
@@ -0,0 +1,41 @@
+from unittest import TestCase
+
+from adapters.mock_book_repo import BookRepo
+from entities.author import Author
+from entities.book import Book
+from .book_search import BookSearchService
+
+
+class TestBookSearch(TestCase):
+    def setUp(self):
+        self.repo = BookRepo()
+        self.book = Book(
+            "title",
+            author=Author("author name"),
+        )
+        self.repo.add_book(self.book)
+        self.book2 = Book(
+            "title2",
+            author=Author("author name"),
+        )
+        self.repo.add_book(self.book2)
+        self.author2_book = Book(
+            "title3",
+            author=Author("author2 name"),
+        )
+        self.repo.add_book(self.author2_book)
+
+    def test_book_get_by_name(self):
+        search_service = BookSearchService(self.repo)
+        book = search_service.get_by_title("title")
+        self.assertEqual(self.book, book)
+
+    def test_book_get_by_name_missing(self):
+        search_service = BookSearchService(self.repo)
+        with self.assertRaises(BookSearchService.NoResults):
+            search_service.get_by_title("title missing")
+
+    def test_list_by_author(self):
+        search_service = BookSearchService(self.repo)
+        books = search_service.list_by_author(Author("author name"))
+        self.assertItemsEqual([self.book, self.book2], books)
